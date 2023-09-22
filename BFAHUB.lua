f9159305e06f07e4d5dde54446166f004ec04e6f
M7MD1x <135571151+M7MD1x@users.noreply.github.com>
2023-09-22 17:52:41 +0300

    Update and rename BFAHUB.txt to BFAHUB.lua

---
+++ BFAHUB.lua
@@ -0,0 +1,9 @@
+local gameid = game.PlaceId
+if(gameid == 2753915549) then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/M7MD1x/BFAHUB/main/BFAHUB-BloxFruits.lua"))()
+end
+
+local gameid = game.PlaceId
+if(gameid == 13772394625) then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/M7MD1x/BFAHUB/main/BFAHUB-BladeBall.lua"))()
+end
